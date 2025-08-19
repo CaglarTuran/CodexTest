182c7064488ddfd0295a043729b3b66b930574cd
Caglar Turan <caglarturan86@hotmail.com>
2025-08-19 16:51:36 +0300

    Add clean architecture Web API project skeleton

---
+++ src/Infrastructure/Services/WeatherForecastService.cs
@@ -0,0 +1,22 @@
+using CleanArchitecture.Application.Interfaces;
+using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Infrastructure.Services;
+
+public class WeatherForecastService : IWeatherForecastService
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    public IEnumerable<WeatherForecast> GetForecast()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        });
+    }
+}
